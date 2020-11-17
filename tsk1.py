d90aaa55be3484001ea86c685f59bbdbb95bf812
Vladyslav Skrypchuk <51884701+Zmeykoo@users.noreply.github.com>
2020-11-17 12:43:11 +0200

    Add files via upload

--- tsk1.py
+++ tsk1.py
@@ -1,1 +1,15 @@
-argh
+from math import sin
+
+fin=[]
+def main():
+	"""Пошук коренів функції"""
+	i=-3.0
+	x=round(i, 3)
+	while x <= 3.0:
+		if round(sin(x),2) - round(x**3,2) == 0:
+			fin.append(x)
+		x=round(x+0.01,2)
+
+main()
+
+print('Корені даної функції:',fin)
